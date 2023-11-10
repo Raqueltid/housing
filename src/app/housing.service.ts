da3006a58f9a296ed0fd029aaa9c249b88bb21d4
rflores_atsistemas <rflores@atsistemas.com>
2023-11-10 11:30:54 +0100

    initial commit with static and http request

---
+++ src/app/housing.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HousingLocation } from "./housinglocation";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HousingService {
+  url = "http://localhost:3000/locations";
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
+  }
+
+  async getHousingLocationById(
+    id: number
+  ): Promise<HousingLocation | undefined> {
+    /* const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
+     */
+    return (await (await fetch(`${this.url}/${id}`)).json()) ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    // tslint:disable-next-line
+    console.log(firstName, lastName, email);
+  }
+}
